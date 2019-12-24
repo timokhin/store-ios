c6726ad05bece377f17a2052359f15aceaae5822
timokhin <tm@likee.ru>
2019-12-24 19:20:14 +0700

    Initial Commit

---
+++ Store/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Store
+//
+//  Created by Maxim Timokhin on 24.12.2019.
+//  Copyright © 2019 GetProger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StoreTests/StoreTests.swift
@@ -0,0 +1,34 @@
+//
+//  StoreTests.swift
+//  StoreTests
+//
+//  Created by Maxim Timokhin on 24.12.2019.
+//  Copyright © 2019 GetProger. All rights reserved.
+//
+
+import XCTest
+@testable import Store
+
+class StoreTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
